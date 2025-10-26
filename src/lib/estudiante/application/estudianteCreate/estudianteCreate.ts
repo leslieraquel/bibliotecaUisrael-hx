d299e4e21f75b0a8a6d3d3331e214cc6307470f3
leslieraquel <raquellezs@hotmail.com>
2025-10-26 00:27:19 -0500

    un archivo

--- src/lib/estudiante/application/estudianteCreate/estudianteCreate.ts
+++ src/lib/estudiante/application/estudianteCreate/estudianteCreate.ts
@@ -5,18 +5,18 @@ import { estudianteCorreo } from "../../domain/estudianteCorreo";
 import { estudianteId } from "../../domain/estudianteId";
 import { estudianteName } from "../../domain/estudianteNombre";
 import { estudianteCarrera } from "../../domain/estudianteCarer";
-
 import { estudianteRepository } from "../../domain/estudianteRepository";
 
 export class estudianteCreate{
     constructor(private repository: estudianteRepository){}
-    async run(id:string, name:string, email:string, createdAt:Date,carrera:string): Promise<void>{
+    async run(id:string, name:string, email:string, createdAt:Date,carrera:string,createUpdate:Date): Promise<void>{
         const estudiante_ = new estudiante(
             new estudianteId(id),
             new estudianteName(name),
             new estudianteCorreo(email),
             new estudianteCreatedAt(createdAt),
-            new estudianteCarrera(carrera)
+            new estudianteCarrera(carrera),
+            new estudianteUpdateAt(createUpdate)
         );
         return this.repository.create(estudiante_);
     }   
